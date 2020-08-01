a5721d8762ebd8d5fc77b560f7262c5ec2dd65fd
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-08-01 18:33:36 -0300

    radio group size fix; unused semicolons removed from script.js

--- client_v2/js/script.js
+++ client_v2/js/script.js
@@ -1,50 +1,50 @@
-let solution;
-let cntSolution = 0;
+let solution
+let cntSolution = 0
 
 function myRound(x, p) {
-  const mult = Math.pow(10, p);
-  return Math.round(mult * x) / mult;
+  const mult = Math.pow(10, p)
+  return Math.round(mult * x) / mult
 }
 
 function downloadCoord(s) {
-  const csvContent = "data:text/csv;charset=utf-8," + s.all_solutions[cntSolution].positions.map(e => e.map(f => myRound(f, 2)).join(",")).join("\n");
-  const encodedUri = encodeURI(csvContent);
-  window.open(encodedUri);
+  const csvContent = "data:text/csv;charset=utf-8," + s.all_solutions[cntSolution].positions.map(e => e.map(f => myRound(f, 2)).join(",")).join("\n")
+  const encodedUri = encodeURI(csvContent)
+  window.open(encodedUri)
 }
 
 function clearRoom(ctx, w, h) {
-  ctx.clearRect(0, 0, w, h);
-  ctx.fillStyle = "#fff";
-  ctx.fillRect(0, 0, w, h);
+  ctx.clearRect(0, 0, w, h)
+  ctx.fillStyle = "#fff"
+  ctx.fillRect(0, 0, w, h)
 }
 
 function drawRoomAndTeacherSpace(ctx, w, h, scale, salax, salay) {
   // Draw room
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = "1";
-  ctx.strokeRect(0, 0, w, h);
+  ctx.strokeStyle = "black"
+  ctx.lineWidth = "1"
+  ctx.strokeRect(0, 0, w, h)
 
-  ctx.strokeStyle = "red";
-  ctx.lineWidth   = "3";
-  ctx.strokeRect(2, h + 2 - salay * scale, salax * scale, salay * scale - 4);
+  ctx.strokeStyle = "red"
+  ctx.lineWidth   = "3"
+  ctx.strokeRect(2, h + 2 - salay * scale, salax * scale, salay * scale - 4)
 
   // Draw teacher space
-  ctx.strokeStyle = "red";
-  ctx.lineWidth = "1";
-  ctx.font = "15px Arial";
-  tw = ctx.measureText("P");
-  ctx.strokeText("P", w - (50 + tw.width) / 2.0, h - (salay * scale) / 2 + 10);
-  ctx.strokeRect(w - 50 + 5, 1, 50 - 5 - 2, h - 2);
+  ctx.strokeStyle = "red"
+  ctx.lineWidth = "1"
+  ctx.font = "15px Arial"
+  tw = ctx.measureText("P")
+  ctx.strokeText("P", w - (50 + tw.width) / 2.0, h - (salay * scale) / 2 + 10)
+  ctx.strokeRect(w - 50 + 5, 1, 50 - 5 - 2, h - 2)
 }
 
 function drawChair(ctx, cw, ch, scale, px, py) {
   // The chair: 2/3 of ch and 1/4 of cw
-  ctx.fillRect(px - (cw / 8) * scale, py - (ch / 3) * scale, cw * scale / 4, (2 * ch)  * scale / 3);
-  ctx.strokeRect(px - (cw / 8) * scale, py - (ch / 3) * scale, cw * scale / 4, (2 * ch)  * scale / 3);
+  ctx.fillRect(px - (cw / 8) * scale, py - (ch / 3) * scale, cw * scale / 4, (2 * ch)  * scale / 3)
+  ctx.strokeRect(px - (cw / 8) * scale, py - (ch / 3) * scale, cw * scale / 4, (2 * ch)  * scale / 3)
 
   // The table: ch and 3/4 of cw
-  ctx.fillRect(px + (ch / 8) * scale, py - (ch / 2) * scale, cw * 3 * scale / 4, ch * scale);
-  ctx.strokeRect(px + (ch / 8) * scale, py - (ch / 2) * scale, cw * 3 * scale / 4, ch * scale);
+  ctx.fillRect(px + (ch / 8) * scale, py - (ch / 2) * scale, cw * 3 * scale / 4, ch * scale)
+  ctx.strokeRect(px + (ch / 8) * scale, py - (ch / 2) * scale, cw * 3 * scale / 4, ch * scale)
 }
 
 /*
@@ -54,172 +54,169 @@ has to be obtained by the optimization packing strategy.
 */
 
 function drawOptSolution(s, move) {
-  if (!s) return;
+  if (!s) return
   
-  cnt = cntSolution + move;
+  cnt = cntSolution + move
   
-  if (cnt < 0 || cnt >= s.all_solutions.length) return;
+  if (cnt < 0 || cnt >= s.all_solutions.length) return
 
-  cntSolution += move;
+  cntSolution += move
 
-  const canvas = document.getElementById("map");
-  __actuallyDrawOptSolution(canvas, s);
-
-  const radius = s.all_solutions[cntSolution].min_distance / 2.0;
-  $("#displayDistance").append(`<h4>Distância: ${Math.round(100 * 2 * radius) / 100.0}</h4>`)
+  const canvas = document.getElementById("map")
+  __actuallyDrawOptSolution(canvas, s)
 
   // Hidden canvas for printing
-  const hiddenCanvas = document.getElementById("map-hidden");
-  __actuallyDrawOptSolution(hiddenCanvas, s);
+  const hiddenCanvas = document.getElementById("map-hidden")
+  __actuallyDrawOptSolution(hiddenCanvas, s)
 }
 
 function download_pdf(filename){
   printJS({
       printable: 'map-hidden',
       type: 'html',
-      style: "#map-hidden {display: block;margin: 0 auto;}"//transform: scale(2.0);margin-top:20%;}"
+      style: "#map-hidden {display: block;margin: 0 auto;}"//transform: scale(2.0)margin-top:20%;}"
   })
 }
 
 /* Internal function to draw the optimization solution in the
 * canvas. */
 function __actuallyDrawOptSolution(mycanvas, s) {
-  const ctx = mycanvas.getContext("2d");
+  const ctx = mycanvas.getContext("2d")
   
-  const w = mycanvas.width*1;
-  const h = mycanvas.height*1;
+  const w = mycanvas.width*1
+  const h = mycanvas.height*1
   
   const salax =  parseFloat($('#txtLarguraSala').val())
   const salay = parseFloat($('#txtComprimentoSala').val())
   const cw = parseFloat($('#txtLarguraCarteira').val())
   const ch = parseFloat($('#txtComprimentoCarteira').val())
   
-  const scale = Math.min((w - 50) / salax, (h) / salay);
+  const scale = Math.min((w - 50) / salax, (h) / salay)
 
-  clearRoom(ctx, w, h);
-  drawRoomAndTeacherSpace(ctx, w, h, scale, salax, salay);
+  clearRoom(ctx, w, h)
+  drawRoomAndTeacherSpace(ctx, w, h, scale, salax, salay)
   
-  currSol = s.all_solutions[cntSolution];
+  currSol = s.all_solutions[cntSolution]
 
-  const radius = currSol.min_distance / 2.0;
-  const fontSize = Math.max(10, parseInt(radius * scale / 2.0));
+  const radius = currSol.min_distance / 2.0
+  const fontSize = Math.max(10, parseInt(radius * scale / 2.0))
   
   for (let i = 0; i < currSol.positions.length; i++) {
-    const x = currSol.positions[i][0] * scale;
-    const y = h - currSol.positions[i][1] * scale;
+    const x = currSol.positions[i][0] * scale
+    const y = h - currSol.positions[i][1] * scale
     
-    ctx.globalAlpha = 0.8;
-    ctx.strokeStyle = "black";
-    ctx.lineWidth   = "1";
+    ctx.globalAlpha = 0.8
+    ctx.strokeStyle = "black"
+    ctx.lineWidth   = "1"
     // Do not paint the chairs
     ctx.fillStyle = "white"
 
     drawChair(ctx, cw, ch, scale, x, y)
 
-    ctx.globalAlpha = 0.7;
-    ctx.fillStyle = "brown";
-    ctx.font = fontSize + "px Arial";
-    tw = ctx.measureText(i + 1);
+    ctx.globalAlpha = 0.7
+    ctx.fillStyle = "brown"
+    ctx.font = fontSize + "px Arial"
+    tw = ctx.measureText(i + 1)
 
-    if (y - (ch * scale) / 2 < fontSize) ctx.fillText(i + 1, x, y + (ch * scale) / 2);
-    else ctx.fillText(i + 1, x, y - (ch * scale) / 2);
+    if (y - (ch * scale) / 2 < fontSize) ctx.fillText(i + 1, x, y + (ch * scale) / 2)
+    else ctx.fillText(i + 1, x, y - (ch * scale) / 2)
     
-    ctx.globalAlpha = 1.0;
+    ctx.globalAlpha = 1.0
   }
 
   // Draw origin
-  ctx.globalAlpha = 0.7;
-  ctx.strokeStyle = "red";
-  ctx.font = "10px Arial";
-  ctx.strokeText("Origem", 2, h - 5);
-  ctx.globalAlpha = 1.0;
+  ctx.globalAlpha = 0.7
+  ctx.strokeStyle = "red"
+  ctx.font = "10px Arial"
+  ctx.strokeText("Origem", 2, h - 5)
+  ctx.globalAlpha = 1.0
 }
 
 function drawRowSol(s) {
-  drawRowSol_Hidden(s);
+  drawRowSol_Hidden(s)
 
-  if (!s) return;
+  if (!s) return
   
-  const mycanvas = document.getElementById("map");
-  const ctx = mycanvas.getContext("2d");
+  const mycanvas = document.getElementById("map")
+  const ctx = mycanvas.getContext("2d")
 
-  const w = mycanvas.width;
-  const h = mycanvas.height;
+  const w = mycanvas.width
+  const h = mycanvas.height
   
   const rW = parseFloat($('#txtLarguraSala').val())
   const rH = parseFloat($('#txtComprimentoSala').val())
   const cW = parseFloat($('#txtLarguraCarteira').val())
   const cH = parseFloat($('#txtComprimentoCarteira').val())
-  const cR = solution.rowSpace;
-  const cC = solution.chairSpace;
+  const cR = solution.rowSpace
+  const cC = solution.chairSpace
 
-  clearRoom(ctx, w, h);
+  clearRoom(ctx, w, h)
 
-  const scale = Math.min((w - 50) / rW, (h) / rH);
-  drawRoomAndTeacherSpace(ctx, w, h, scale, rW, rH);
+  const scale = Math.min((w - 50) / rW, (h) / rH)
+  drawRoomAndTeacherSpace(ctx, w, h, scale, rW, rH)
 
-  ctx.globalAlpha = 0.8;
-  ctx.strokeStyle = "black";
-  ctx.lineWidth   = "1";
-  ctx.fillStyle   = "#BB8888";
+  ctx.globalAlpha = 0.8
+  ctx.strokeStyle = "black"
+  ctx.lineWidth   = "1"
+  ctx.fillStyle   = "#BB8888"
 
-  let py = h - rH * scale + cH * scale / 2;
+  let py = h - rH * scale + cH * scale / 2
 
   for (let i = 0; i < solution.rows; i++) {
-    let px = cW * scale / 4;
+    let px = cW * scale / 4
     
     for (let j = 0; j < solution.chairs; j++) {
         ctx.fillStyle = "white"
         if (solution.A[i][j] == 1) ctx.fillStyle = "black"
 
         drawChair(ctx, cW, cH, scale, px, py)
-        px += (cW + cC) * scale;
+        px += (cW + cC) * scale
     }
 
-    py += (cH + cR) * scale;
+    py += (cH + cR) * scale
   }
 }
 
 function drawRowSol_Hidden(s) {
-  if (!s) return;
+  if (!s) return
   
-  const mycanvas = document.getElementById("map-hidden");
-  const ctx = mycanvas.getContext("2d");
+  const mycanvas = document.getElementById("map-hidden")
+  const ctx = mycanvas.getContext("2d")
 
-  const w = mycanvas.width;
-  const h = mycanvas.height;
+  const w = mycanvas.width
+  const h = mycanvas.height
   
   const rW = parseFloat($('#txtLarguraSala').val()) * 1.5
   const rH = parseFloat($('#txtComprimentoSala').val()) * 1.5
   const cW = parseFloat($('#txtLarguraCarteira').val()) * 1.5
   const cH = parseFloat($('#txtComprimentoCarteira').val()) * 1.5
-  const cR = solution.rowSpace*1.5;
-  const cC = solution.chairSpace*1.5;
+  const cR = solution.rowSpace*1.5
+  const cC = solution.chairSpace*1.5
 
-  clearRoom(ctx, w, h);
+  clearRoom(ctx, w, h)
 
-  const scale = Math.min((w - 50) / rW, (h) / rH);
-  drawRoomAndTeacherSpace(ctx, w, h, scale, rW, rH);
+  const scale = Math.min((w - 50) / rW, (h) / rH)
+  drawRoomAndTeacherSpace(ctx, w, h, scale, rW, rH)
 
-  ctx.globalAlpha = 0.8;
-  ctx.strokeStyle = "black";
-  ctx.lineWidth   = "1";
-  ctx.fillStyle   = "#BB8888";
+  ctx.globalAlpha = 0.8
+  ctx.strokeStyle = "black"
+  ctx.lineWidth   = "1"
+  ctx.fillStyle   = "#BB8888"
 
-  let py = h - rH * scale + cH * scale / 2;
+  let py = h - rH * scale + cH * scale / 2
 
   for (let i = 0; i < solution.rows; i++) {
-      let px = cW * scale / 4;
+      let px = cW * scale / 4
       
       for (let j = 0; j < solution.chairs; j++) {
-          ctx.fillStyle = "white";
-          if (solution.A[i][j] == 1) ctx.fillStyle = "black";
+          ctx.fillStyle = "white"
+          if (solution.A[i][j] == 1) ctx.fillStyle = "black"
 
-          drawChair(ctx, cW, cH, scale, px, py);
-          px += (cW + cC) * scale;
+          drawChair(ctx, cW, cH, scale, px, py)
+          px += (cW + cC) * scale
       }
 
-      py += (cH + cR) * scale;
+      py += (cH + cR) * scale
   }
 }
 
@@ -229,7 +226,7 @@ function prepareResultsSection() {
   $("#result").append('<div id="summary" class="col-sm-12 col-lg-6 mt-4 mb-4"></div>')
   $("#sectionSeparator").show()
   $("#sectionResults").show()
-  $("#result").append('<img src="assets/img/loading.gif" id="loading"></img>');
+  $("#result").append('<img src="assets/img/loading.gif" id="loading"></img>')
 }
 
 function drawFixedLayout(result) {
@@ -264,7 +261,7 @@ function drawFreeLayout(result) {
       <button class="btn btn-success" onclick="drawOptSolution(solution, +1)">&gt;</button>
       <button class="btn btn-confirm" onclick="downloadCoord(solution)">Baixar Coordenadas (CSV)</button>
     </div>
-  `);
+  `)
 
   solution = result
   cntSolution = 0
@@ -339,7 +336,7 @@ $(document).ready(function() {
     //   url: url,
     //   data: form.serialize(),
     //   success: function(data) {
-    //       alert(data);
+    //       alert(data)
     //   }
     // })
   })
